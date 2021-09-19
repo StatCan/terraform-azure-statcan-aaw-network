6317349c19ce2e444b80d2d3a6eb48503725f1d7
Blair Drummond <blair.drummond@cloud.statcan.ca>
2021-09-19 18:34:28 +0000

    Add bitnami.github.io to argocd resources

--- firewall.tf
+++ firewall.tf
@@ -374,7 +374,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "daaas" {
     # To allow Argo CD deployments to read deployment resources
     rule {
       name              = "argocd-deployment-resources"
-      destination_fqdns = ["github.com", "statcan.github.io"]
+      destination_fqdns = ["github.com", "statcan.github.io", "bitnami.github.io"]
       source_addresses  = azurerm_subnet.aks_system.address_prefixes
 
       protocols {
