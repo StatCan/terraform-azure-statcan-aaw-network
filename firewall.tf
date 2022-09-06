5d679b7e3cf243b27fdceefec6e83ec9c0f632d8
Collin Brown <collin.brown@cloud.statcan.ca>
2022-09-06 16:07:03 +0000

    fix(firewall.tf): protocol block instead of list of ports/protocols

--- firewall.tf
+++ firewall.tf
@@ -568,8 +568,14 @@ resource "azurerm_firewall_policy_rule_collection_group" "cloud_main_system" {
       destination_addresses = [var.cloud_main_gitlab_ssh_ip, var.cloud_main_gitlab_https_ip]
       source_addresses  = [azurerm_subnet.aks_cloud_main_system]
       # Users might interact with gitlab over https or ssh
-      protocols         = ["TCP"]
-      ports             = ["22", "443"]
+      protocols {
+        port = 22
+        type = "ssh"
+      }
+      protocols {
+        port = 443
+        type = "Https"
+      }
     }
   }
 }
